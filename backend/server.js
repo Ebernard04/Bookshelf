da128a00798a11b3a620cf45c0454c3d9ad75a6b
Eric <infinitebomb@gmail.com>
2026-03-28 17:05:56 -0500

    route stuff

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-//app.use('/api/books', require('./routes/books'));
+app.use('/api/books', require('./routes/books'));
 app.use('/api/authors', require('./routes/authors'));
-//app.use('/api/series', require('./routes/series'));
-//app.use('/api/universes', require('./routes/universes'));
-//app.use('/api/reading-log', require('./routes/reading-log'));
+app.use('/api/series', require('./routes/series'));
+app.use('/api/universes', require('./routes/universes'));
+app.use('/api/reading-log', require('./routes/reading-log'));
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
