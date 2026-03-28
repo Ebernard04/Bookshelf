d75b22bf9de72ee0522e95ced800b37c40e908ac
Eric <infinitebomb@gmail.com>
2026-03-28 17:09:11 -0500

    route stuff

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use('/api/books', require('./routes/books'));
 app.use('/api/authors', require('./routes/authors'));
 app.use('/api/series', require('./routes/series'));
 app.use('/api/universes', require('./routes/universes'));
-app.use('/api/reading-log', require('./routes/reading-log'));
+app.use('/api/reading-log', require('./routes/readingLog'));
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
