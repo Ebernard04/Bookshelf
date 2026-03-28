7856a0956d225db1107bba728f70ee2b477c6ba7
Eric <infinitebomb@gmail.com>
2026-03-28 14:30:01 -0500

    temp remove routes

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/books', require('./routes/books'));
-app.use('/api/authors', require('./routes/authors'));
-app.use('/api/series', require('./routes/series'));
-app.use('/api/universes', require('./routes/universes'));
-app.use('/api/reading-log', require('./routes/reading-log'));
+//app.use('/api/authors', require('./routes/authors'));
+//app.use('/api/series', require('./routes/series'));
+//app.use('/api/universes', require('./routes/universes'));
+//app.use('/api/reading-log', require('./routes/reading-log'));
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
